2a415df30a9d2981968c5c2f0a40b57339a24dab
Paul Dreik <github@pauldreik.se>
2019-04-06 17:54:16 +0200

    improve the fuzzer script,

--- fuzzers/doit.sh
+++ fuzzers/doit.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-
 #run this from the build directory
 
 set -e
 
-rootdir=$(readlink -f $(dirname $0)/..)
+rootdir=$(readlink -f "$(dirname "$0")/..")
 
 #where is the corpus directory
 corpusdir=$rootdir/fuzzers/corpus
@@ -13,36 +12,54 @@ corpusdir=$rootdir/fuzzers/corpus
 builddir=$(pwd)
 
 #which mode to run in  (minimize|run|reproduce)
-mode=minimize
-mode=run
-mode=reproduce
+#mode=libfuzzerminimize
+#mode=run
+#mode=reproduce
+#mode=runafl
+#mode=runaflcmin
+mode=runlibfuzzerinscreen
 
 for fuzzer in $(ls fuzzer_* |sed -e 's/fuzzer_//') ; do
-   echo $looking at fuzzer $fuzzer
+    echo $looking at fuzzer $fuzzer
 
-   mkdir -p out/$fuzzer
-   mkdir -p $corpusdir/$fuzzer
-   export UBSAN_OPTIONS=halt_on_error=1
+    mkdir -p out/$fuzzer
+    mkdir -p $corpusdir/$fuzzer
+    export UBSAN_OPTIONS=halt_on_error=1
 
-   #running locally
-   if [ $mode = run ] ; then
-    $builddir/fuzzer_$fuzzer -max_total_time=20 out/$fuzzer $corpusdir/$fuzzer
-   fi
+    #running locally
+    if [ $mode = run ] ; then
+	$builddir/fuzzer_$fuzzer -max_total_time=20 out/$fuzzer $corpusdir/$fuzzer
+    fi
 
-   #minimization of corpus. replace the existing one.
-   if [ $mode = minimize ]; then
-   [ -e $corpusdir/$fuzzer.tmp ] && rm -rf $corpusdir/$fuzzer.tmp
-   mkdir -p $corpusdir/$fuzzer.tmp
-   $builddir/fuzzer_$fuzzer -merge=1 $corpusdir/$fuzzer.tmp $corpusdir/$fuzzer out/$fuzzer
-   rm -rf $corpusdir/$fuzzer
-   mv $corpusdir/$fuzzer.tmp $corpusdir/$fuzzer
-   fi
+    #minimization of corpus. replace the existing one.
+    if [ $mode = libfuzzerminimize ]; then
+	[ -e $corpusdir/$fuzzer.tmp ] && rm -rf $corpusdir/$fuzzer.tmp
+	mkdir -p $corpusdir/$fuzzer.tmp
+	$builddir/fuzzer_$fuzzer -merge=1 $corpusdir/$fuzzer.tmp $corpusdir/$fuzzer out/$fuzzer
+	rm -rf $corpusdir/$fuzzer
+	mv $corpusdir/$fuzzer.tmp $corpusdir/$fuzzer
+    fi
 
-   #reproduce - just runs the input from the corpus
-   if [ $mode = reproduce ]; then
-   $builddir/fuzzer_$fuzzer $corpusdir/$fuzzer/*
-   fi
+    #reproduce - just runs the input from the corpus
+    if [ $mode = reproduce ]; then
+	$builddir/fuzzer_$fuzzer $corpusdir/$fuzzer/*
+    fi
+
+    #running afl in separate screen sessions
+    if [ $mode = runafl ]; then
+	screen -S afl -X screen
+	screen -S afl -X stuff "afl-fuzz -m none -i cminout/$fuzzer -o out/$fuzzer -- $builddir/fuzzer_$fuzzer @@\n"
+    fi
+    if [ $mode = runaflcmin ]; then
+	mkdir -p cminout/$fuzzer
+	screen -S afl -X screen
+	screen -S afl -X stuff "afl-cmin -m none -i $corpusdir/$fuzzer -o cminout/$fuzzer -- $builddir/fuzzer_$fuzzer @@\n"
+    fi
+
+    if [ $mode = runlibfuzzerinscreen ]; then
+	screen -S libfuzzer -X screen
+	screen -S libfuzzer -X title "libfuzzer run $(LANG= date --rfc-3339=s) run"
+	screen -S libfuzzer -X stuff "$builddir/fuzzer_$fuzzer out/$fuzzer $corpusdir/$fuzzer\n"
+    fi
+
 done
-
-
-
